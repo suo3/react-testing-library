5fc9f9a6b0429c6fb3315f529b96eccbd2f58ad2
Samuel Osseo-Asare <sasare@healthadvocate.com>
2024-05-01 12:24:11 -0400

    add drinks search

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Vote from "./vote";
 import "./App.css";
 import { Container } from "react-bootstrap";
 import EmployeeEmail from "./employeeEmail";
+import DrinkSearch from "./drinkSearch";
 
 function App() {
   return (
     <Container className='App'>
       <EmployeeEmail />
       <Vote totalGlobalLikes={0} />
+      <DrinkSearch />
     </Container>
   );
 }
